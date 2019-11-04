b0f7d45479fe83771c725c69d779b903aa31b326
Raymond Camden <raymondcamden@gmail.com>
2019-11-04 13:05:33 -0600

    added some comments to the script

--- xyz_search/stats.js
+++ xyz_search/stats.js
@@ -1,9 +1,15 @@
+/*
+
+This script runs the statistics endpoint on a hard coded space (should be moved to .env, will do so now)
+which reports on properties and their searchability.
+*/
+
 require('dotenv').config();
 
 const fetch = require('node-fetch');
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
-const SPACE_ID = 'ZCtPqZJA';
+const SPACE_ID = process.env.SPACE_ID;
 
 fetch(`https://xyz.api.here.com/hub/spaces/${SPACE_ID}/statistics?access_token=${ACCESS_TOKEN}`)
 .then(res => res.json())
