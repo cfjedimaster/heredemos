bf2ca2c476d9e9acdec2767f5aaa8aed8987737c
Raymond Camden <raymondcamden@gmail.com>
2020-03-17 17:00:42 -0500

    forgot to save

--- harpgl/index6.js
+++ harpgl/index6.js
@@ -16,11 +16,7 @@ const omvDataSource = new harp.OmvDataSource({
 });
 map.addDataSource(omvDataSource);
 
-/*
-map.setCameraGeolocationAndZoom(
-    new harp.GeoCoordinates(30.22, -92.02), 14 
-);
-*/
+
 
 const options = { tilt: 45, distance: 3000 };
 const coordinates = new harp.GeoCoordinates(30.22, -92.02);
