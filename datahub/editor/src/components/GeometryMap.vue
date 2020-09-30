c562bfa9df86702a4d756570f76babd4e52e09c4
Raymond Camden <raymondcamden@gmail.com>
2020-09-30 15:00:05 -0500

    interim checkin

---
+++ datahub/editor/src/components/GeometryMap.vue
@@ -0,0 +1,67 @@
+<template>
+  <img v-if="imgSrc" :src="imgSrc">
+</template>
+
+<script>
+
+//todo - remove :)
+const key = 'c1LJuR0Bl2y02PefaQ2d8PvPnBKEN8KdhAOFYR_Bgmw';
+
+export default {
+	name: 'GeometryMap',
+	props:{
+		geometry: { required: true },
+		width: { default: 500 },
+		height: { default: 500 }
+	},
+	created() {
+		console.log('wtf', this.geometry);
+		console.log(this.geometry);
+		this.imgSrc = this.featureToMap(this.geometry, key, this.width, this.height);
+		console.log('done', this.imgSrc);
+	},
+	data: () => ({
+		imgSrc:null
+	}),
+	methods:{
+		featureToMap(geometry,key,w=500,h=500) {
+			let url = 'https://image.maps.ls.hereapi.com/mia/1.6/';
+			if(geometry.type === 'Point') {
+				url += `mapview?apiKey=${key}&w=${w}&h=${h}`;
+				url += `&c=${geometry.coordinates[1]},${geometry.coordinates[0]}`;
+			} else if(geometry.type === 'MultiPoint') {
+				url += `mapview?apiKey=${key}&w=${w}&h=${h}&poi=`;
+				for(let i=0;i<geometry.coordinates[0].length;i++) {
+					url += `${geometry.coordinates[i][1]},${geometry.coordinates[i][0]}`;
+					if(i < geometry.coordinates.length-1) {
+						url += ',';
+					}
+				}
+			} else if(geometry.type === 'LineString') {
+				url += `route?apiKey=${key}&w=${w}&h=${h}`;
+				url += '&r0=';
+				for(let i=0;i<geometry.coordinates.length;i++) {
+					url += `${geometry.coordinates[i][1]},${geometry.coordinates[i][0]}`;
+					if(i < geometry.coordinates.length-1) {
+						url += ',';
+					}
+				}
+			} else if(geometry.type === 'Polygon') {
+				url += `region?apiKey=${key}&w=${w}&h=${h}`;
+				url += '&a=';
+				for(let i=0;i<geometry.coordinates[0].length;i++) {
+					url += `${geometry.coordinates[0][i][1]},${geometry.coordinates[0][i][0]}`;
+					if(i < geometry.coordinates[0].length-1) {
+						url += ',';
+					}
+				}
+			} else {
+				return;
+			}
+			return url;
+			
+		}
+
+	}
+}
+</script>
--- datahub/editor/src/views/Space.vue
+++ datahub/editor/src/views/Space.vue
@@ -1,18 +1,41 @@
 <template>
   <v-container>
     <h2>Space {{ space.title }} <span v-if="features">({{ features.length }} features)</span></h2>
+
+    <v-card v-for="feature in features" :key="feature.id" class="mb-6" elevation="10">
+      <v-card-title>Feature: {{feature.id}}</v-card-title>
+      <v-card-text>
+        <v-textarea 
+          label="Properties" :value="jsonPrint(feature.properties)"
+          background-color="amber lighten-4" rows="10" row-height="30"
+        ></v-textarea>
+        <v-textarea 
+          label="Geometry" :value="jsonPrint(feature.geometry)"
+          v-model="feature.geometry"
+          background-color="amber lighten-4" rows="10" row-height="30"
+        ></v-textarea>
+        <GeometryMap :geometry="feature.geometry" width="400" height="300"></GeometryMap>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="green" text :click="saveFeature(feature)">Save Changes</v-btn>
+      </v-card-actions>
+    </v-card>
+
   </v-container>
 </template>
 
 <script>
+import GeometryMap from '@/components/GeometryMap';
+
 export default {
+  components: { GeometryMap },
   created() {
-	this.space = this.$route.params.space;
+    this.space = this.$route.params.space;
     this.$store.dispatch('loadSpace', { space:this.space });
   },
   data() {
     return {
-		space: null
+  		space: null
     }
   },
   computed:{
@@ -21,6 +44,11 @@ export default {
     }
   },
   methods: {
+    jsonPrint(s) { return JSON.stringify(s, '', '\t'); },
+    saveFeature(f) {
+      console.log('lets save this feature');
+      console.log(f.propeties)
+    }
   }
 }
 </script>
